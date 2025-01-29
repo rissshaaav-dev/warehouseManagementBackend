3cd5f16e9c2def65d12e485f6955f4879958bf88
mishrishav <191943910+mishrishav@users.noreply.github.com>
2025-01-29 19:12:42 +0530

    -Create auth controller to handle signup and login

---
+++ controllers/auth.controller.js
@@ -0,0 +1,97 @@
+const User = require("../models/User.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  try {
+    // check if all necessary fields are supplied
+    if (!name || !email || !password) {
+      const error = new Error(
+        "Necessary fields missing: name, email, password"
+      );
+      error.statusCode = 400;
+      throw error;
+    }
+
+    // check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const error = new Error("User already exists");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    // encrypt password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // crate new user
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    // generate jwt token
+    const token = jwt.sign(
+      { id: newUser._id, role: newUser.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "10d",
+      }
+    );
+
+    // send success response
+    res.status(201).json({ message: "User created successfully", token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // check if all necessary fields are supplied
+    if (!email || !password) {
+      const error = new Error("Necessary fields missing: email, password");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    // check if user exists in database
+    const retrievedUser = await User.findOne({ email });
+    if (!retrievedUser) {
+      const error = new Error("User not found");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    // match passwords
+    const isMatch = await bcrypt.compare(password, retrievedUser.password);
+    if (!isMatch) {
+      const error = new Error("Invalid credentials");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    // generate jwt token
+    const token = jwt.sign(
+      { id: newUser._id, role: retrievedUser.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "10d",
+      }
+    );
+
+    // send response
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { registerUser, loginUser };
