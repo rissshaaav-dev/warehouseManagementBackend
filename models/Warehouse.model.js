00a2a390064f0f4710e520e4d559d3f5d702299b
mishrishav <191943910+mishrishav@users.noreply.github.com>
2025-01-30 01:34:33 +0530

    -Modify warehouse model to include timestamps

--- models/Warehouse.model.js
+++ models/Warehouse.model.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 const WarehouseSchema = new mongoose.Schema({
   name: String,
   location: String,
-});
+}, {timestamps: true});
 
 module.exports = mongoose.model("Warehouse", WarehouseSchema);
