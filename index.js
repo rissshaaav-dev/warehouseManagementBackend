6590320d29f9adeefa5d07bc636cef39026aa741
mishrishav <191943910+mishrishav@users.noreply.github.com>
2025-01-29 20:08:45 +0530

    -Add console log in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db.config");
 const errorHandler = require("./middlewares/errorHandler.middleware");
+const authRouter = require("./routes/auth.routes");
 
 const app = express();
 
@@ -15,10 +16,13 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.use("/api/auth", authRouter);
+
 // Error handling middleware (must be at the end)
 app.use(errorHandler);
 
 // Starting server
 app.listen(process.env.PORT, () => {
   console.log(`🚀 Server running on port ${process.env.PORT}`);
+  console.log(`Connecting to mongodb...`);
 });
