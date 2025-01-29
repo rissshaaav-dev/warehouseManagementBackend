fb9d156fe8e6285f1e835749b974271a7368d4cb
mishrishav <191943910+mishrishav@users.noreply.github.com>
2025-01-29 16:41:43 +0530

    -Create User model

---
+++ models/User.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  role: {
+    type: String,
+    enum: ["admin", "warehouse-manager", "staff"],
+    default: "staff",
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
